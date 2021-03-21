d907cd8e90b9bf6203b799fa28ab944d59666283
rinomari <ysp622@gmail.com>
2021-03-21 13:59:14 +0900

    component

--- src/components/ExplorationSection.js
+++ src/components/ExplorationSection.js
@@ -1,10 +1,26 @@
-class ExplorationBar{
-    constructor($element){
+export default class ExplorationSection{
+    constructor({$element,data}){
+        this.data = data;
+        this.explSection = document.createElement('h4');
+        this.explSection.className = 'explSection';
+        
+        document.querySelector('.content').appendChild(this.explSection);
         this.render();
     }
 
-    
+    setState(data){
+        this.data = data;
+        this.render();
+    }
+
     render(){
-        
+        const rootFlg = null;
+        const subTitle = `<span>root</span>`;
+
+        if(rootFlg == null){
+            this.explSection.innerHTML = subTitle;
+        }else{
+            this.explSection.innerHTML = subTitle;
+        }
     }
 }
